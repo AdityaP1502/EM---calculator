3780f05276c10d243936413e14269a8f8e9c3705
AdityaP1502 <i.m.aditya015@gmail.com>
2022-10-07 17:06:26 +0700

    Routine: Implement read routine method for mode 3

--- Routine/Routine.py
+++ Routine/Routine.py
@@ -1,4 +1,5 @@
 from Math.Calculator import Calculator
+from Math.Vector import Vector
 from UI.UI import UI
 from Medium import Medium
 
@@ -57,7 +58,6 @@ class Routine():
       
     readInterReflection()
     
-    
     return reflection, inter_reflection
   
   @staticmethod
@@ -71,14 +71,26 @@ class Routine():
     mag, ampl = map(lambda x: float(x), ampl_str)
     
     return Calculator.toRect(mag, ampl, "degrees")
-   
-  @staticmethod 
-  def __readIncidentWavePolarized():
-    pass
   
   @staticmethod
-  def __readDirectionVector():
-    pass
+  def __readVector(ui : UI, vector_name : str):
+    data_name = "Vector {}".format(vector_name)
+    parameters = [["x component", ""], ["y component", ""], ["z component", ""]]
+    extra_info_prepend = ""
+    extra_info_append = ""
+    
+    vector_component_str = ui.getData(data_name, extra_info_prepend, extra_info_append, parameters)
+    vector_component = map(lambda x: float(x), vector_component_str)
+    
+    return Vector(*vector_component)
+  
+  @staticmethod 
+  def __readIncidentWavePolarized(ui : UI) -> Vector:
+    return Routine.__readVector(ui, "Electric Field")
+  
+  @staticmethod
+  def __readDirectionVector(ui : UI) -> Vector:
+    return Routine.__readVector(ui, "Wave Direction")
   
   @staticmethod
   def __Mode1(ui : UI):
@@ -97,8 +109,28 @@ class Routine():
     return n, ampl, mediums
   
   @staticmethod
-  def __Mode3():
-    pass
+  def __readBoundaryPlane(ui) -> Vector:
+    data_name = "Boundary Equation"
+    parameters = [["A", "x coef"], ["B", "y coef"], ["C", "z coef"]]
+    extra_info_prepend = "(Ax+By+Cz = 0)"
+    extra_info_append = ""
+    
+    plane_component_str = ui.getData(data_name, extra_info_prepend, extra_info_append, parameters)
+    plane_component = map(lambda x: float(x), plane_component_str)
+    
+    # return the normal vector of the plane
+    return Vector(*plane_component)
+  
+  @staticmethod
+  def __Mode3(ui : UI):
+    n = 2 # only support for two medium
+    omega = Routine.__readOmega(ui)
+    electricField = Routine.__readIncidentWavePolarized(ui)
+    mediums = Routine.__readMedium(ui, n, omega) 
+    dir_vector = Routine.__readDirectionVector(ui)
+    boundary_normal_vector = Routine.__readBoundaryPlane(ui)
+    
+    return mediums, electricField, dir_vector, boundary_normal_vector
   
   @staticmethod
   def init() -> list[float]:
@@ -115,7 +147,11 @@ class Routine():
 
     # find type of calculation
     prompt = "Choose calculation mode below:"
-    options = ["Find reflected and transmitted wave given reflection coef", "reflected and transmitted wave full calculation"]
+    options = [
+      "Find reflected and transmitted wave given reflection coef", 
+      "Reflected and transmitted wave full calculation", 
+      "Oblique Incidence"
+      ]
     mode = ui.getOptions(prompt, options)
     
     if (mode == 1):
@@ -124,6 +160,8 @@ class Routine():
     elif (mode == 2):
       data = Routine.__Mode2(ui)
       
+    elif (mode == 3):
+      data = Routine.__Mode3(ui)
       
     return mode, list(data)
   
