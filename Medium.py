2826929cb4b423178a2031949fc89ada0888086a
AdityaP1502 <i.m.aditya015@gmail.com>
2022-10-07 23:00:34 +0700

    Medium: Minor update in param

--- Medium.py
+++ Medium.py
@@ -1,6 +1,6 @@
 from Math.Calculator import Calculator
 class Medium():
-  param = [["er", "Relative Permitivity"], ["mr", "Relative Permeability"], ["sigma", "Conductivity"], ["distance", "(on lambda) : Ex. 0.1 => 0.1 * lambda"]]
+  param = [["er", "Relative Permitivity"], ["mr", "Relative Permeability"], ["sigma", "Conductivity (for perfect conductor insert infinity)"], ["distance", "(on lambda) : Ex. 0.1 => 0.1 * lambda. 0 if on the edge"]]
   def __init__(self, er, mr, sigma, dist, omega) -> None:
     self.propagation = Calculator.calculatePropagation(er, mr, sigma, omega) 
     self.resistance = Calculator.calculateResistance(er, mr, sigma, omega)
